da3ae075c1abb28218bd1a55d2ef39af13748f9f
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-10-27 20:17:31 +0900

    problem1: isException 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,13 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+function isException(pages) {
+  if (isNaN(pages[0]) || isNaN(pages[1])) return true;
+  else if (pages[0] <= 1 || pages[0] >= 400 || pages[1] <= 1 || pages[1] >= 400)
+    return true;
+  else if (pages[0] % 2 === 0 || pages[1] % 2 !== 0) return true;
+  else if (pages[0] + 1 !== pages[1]) return true;
+  else return false;
+}
+
 module.exports = problem1;
