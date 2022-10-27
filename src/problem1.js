b2d9d0bec75d640ef8b81f72f294fd3b7f417c26
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-10-27 20:23:47 +0900

    problem1: multiplyDigits 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -22,4 +22,14 @@ function sumDigits(pageNumber) {
   return sum;
 }
 
+function multiplyDigits(pageNumber) {
+  let product = 1;
+  let tmp = pageNumber;
+  while (tmp > 0) {
+    product *= tmp % 10;
+    tmp = Math.floor(tmp / 10);
+  }
+  return product;
+}
+
 module.exports = problem1;
