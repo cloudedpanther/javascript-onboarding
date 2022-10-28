0b36b73464296a2b6a8e848ab64187e210c8a5dc
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-10-28 15:07:12 +0900

    problem3: 기능 수정

--- src/modules/problem3/countByAllDigits.js
+++ src/modules/problem3/countByAllDigits.js
@@ -2,3 +2,5 @@ function countByAllDigits(digitLevel) {
   if (digitLevel === 1) return 3;
   return countByAllDigits(digitLevel - 1) * 9 + Math.pow(10, digitLevel - 1) * 3;
 }
+
+module.exports = countByAllDigits;
