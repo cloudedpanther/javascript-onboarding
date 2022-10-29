f993d41297cabe15b3fff2c841b377ebf0e5de5c
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-10-30 01:17:07 +0900

    problem3: countToLevel 변수 범위 수정

--- src/modules/problem3/countToLevel.js
+++ src/modules/problem3/countToLevel.js
@@ -2,8 +2,8 @@ function countToLevel(level) {
   if (level < 1) return [0];
 
   const clapsToLevel = [0, 3];
-  for (let i = 1; i < level; i++) {
-    clapsToLevel.push(10 * clapsToLevel[i - 1] + 3 * Math.pow(10, i));
+  for (let i = 2; i <= level; i++) {
+    clapsToLevel.push(10 * clapsToLevel[i - 1] + 3 * Math.pow(10, i - 1));
   }
   return clapsToLevel;
 }
