56e8bd5ee3b714e191b90689772af2f2d3c7f535
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-10-30 01:01:07 +0900

    problem3: countToLevel 반환값 수정

--- src/modules/problem3/countToLevel.js
+++ src/modules/problem3/countToLevel.js
@@ -1,7 +1,7 @@
 function countToLevel(level) {
-  if (level < 1) return [];
+  if (level < 1) return [0];
 
-  const clapsToLevel = [3];
+  const clapsToLevel = [0, 3];
   for (let i = 1; i < level; i++) {
     clapsToLevel.push(10 * clapsToLevel[i - 1] + 3 * Math.pow(10, i));
   }
