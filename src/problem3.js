c03910d5de43958203cfed6ccd57b5ff81c5271a
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-10-30 01:25:04 +0900

    problem3: 기능 요구사항 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,10 @@
-function problem3(number) {}
+const countToLevel = require("./modules/problem3/countToLevel.js");
+const countAll = require("./modules/problem3/countAll.js");
+
+function problem3(number) {
+  const level = number.toString().length;
+  const clapsToLevel = countToLevel(level - 1);
+  return countAll(number, clapsToLevel);
+}
 
 module.exports = problem3;
