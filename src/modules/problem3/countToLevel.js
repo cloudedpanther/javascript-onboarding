4781d838aec374793bde5c81dd2e72e020046273
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-10-30 00:48:08 +0900

    problem3: 기능 추가

---
+++ src/modules/problem3/countToLevel.js
@@ -0,0 +1,9 @@
+function countToLevel(level) {
+  if (level < 1) return -1;
+
+  const clapsToLevel = [3];
+  for (let i = 1; i < level; i++) {
+    clapsToLevel.push(10 * clapsToLevel[i - 1] + 3 * Math.pow(10, i));
+  }
+  return clapsToLevel;
+}
